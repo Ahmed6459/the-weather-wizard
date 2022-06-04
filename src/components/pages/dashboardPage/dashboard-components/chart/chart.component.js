aa9bb57894229b733982e3c327e6bcb35b8e8b5e
mona9855 <salaheltantawy@gmail.com>
2022-06-04 14:42:31 +0200

    chart

--- src/components/pages/dashboardPage/dashboard-components/chart/chart.component.js
+++ src/components/pages/dashboardPage/dashboard-components/chart/chart.component.js
@@ -1,45 +1,84 @@
 import React from "react";
 import "./chart.style.css";
-import { BarChart, XYPlot, HorizontalGridLines } from "react-d3-components";
+import * as d3 from "d3";
 
 const Chart = ({ weatherData }) => {
   const length = weatherData.ClimateAverages[0].month.length;
-  let tempPerMonth = [{}];
+  let temp = [];
+  let months = [];
 
   for (let i = 0; i < length; i++) {
-    tempPerMonth[i] = {
-      x: weatherData.ClimateAverages[0].month[i].name,
-      y: weatherData.ClimateAverages[0].month[i].absMaxTemp,
-    };
+    temp[i] =
+      parseInt(weatherData.ClimateAverages[0].month[i].absMaxTemp, 10) * 5;
+    months[i] = weatherData.ClimateAverages[0].month[i].name;
   }
 
-  console.log(tempPerMonth);
+  //here:
+  var svgWidth = 700;
+  var svgHeight = 250;
 
-  let data = [
-    {
-      // values: [
-      //   { x: "18-24", y: 7000 },
-      //   { x: "25-34", y: 31000 },
-      //   { x: "35-44", y: 34500 },
-      //   { x: "45-55", y: 25000 },
-      //   { x: "55-64", y: 7500 }
-      // ]
-      values: tempPerMonth,
-    },
-  ];
+  var svg = d3
+    .select("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+    .attr("class", "bar-chart");
+
+  // var dataset = [80, 100, 56, 120, 180, 30, 40, 120, 160];
+  var dataset = temp;
+
+  var barPadding = 5;
+  var barWidth = svgWidth / dataset.length;
+
+  var barChart = svg
+    .selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("y", function(d) {
+      return svgHeight - d;
+    })
+    .attr("height", function(d) {
+      return d;
+    })
+    .attr("width", barWidth - barPadding)
+    .attr("transform", function(d, i) {
+      var translate = [barWidth * i, 0];
+      return "translate(" + translate + ")";
+    });
 
   return (
     <div className="chart">
       <h1 className="title">Temprature Per Month</h1>
 
-      <BarChart
-        data={data}
-        width={347}
-        height={307}
-        margin={{ top: 10, bottom: 50, left: 50, right: 0 }}
-      />
+      <svg></svg>
+     
+      <div className="month-container">
+        {months.map((month) => {
+          return (
+            <div className="month-item" key={month}>
+              {month}
+            </div>
+          );
+        })}
+      </div>
+      {
+        <div className="temp-all">
+          <div className="temp-container">
+            {temp.map((item) => {
+              return (
+                <div className="temp-item" key={item}>
+                  {item/5}&#8451;
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      }
+
+     
     </div>
   );
 };
 
+// npm install webpack@4.19.1
 export default Chart;
